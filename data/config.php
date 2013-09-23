5d408380f700ec335f2b25cde18fb1ce16b88eff
boxcore <boxcore@f188abb9-0070-40a5-a00a-d48f6c791f86>
2013-09-23 09:26:56 +0000

    初始商城环境
    git-svn-id: http://www.svnchina.com/svn/noface@1 f188abb9-0070-40a5-a00a-d48f6c791f86

---
+++ data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ohmyface";
+
+// database username
+$db_user   = "ohmyface";
+
+// database password
+$db_pass   = "ohmyface";
+
+// table prefix
+$prefix    = "yzl_";
+
+$timezone    = "Asia/Chongqing";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2013-09-23 17:07:58');
+
+?>
