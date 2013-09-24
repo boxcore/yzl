cb3fe7eee5a2923ad201d6485c8914bb1a5d8924
boxcore <boxcore@f188abb9-0070-40a5-a00a-d48f6c791f86>
2013-09-24 00:49:58 +0000

    git-svn-id: http://www.svnchina.com/svn/noface@5 f188abb9-0070-40a5-a00a-d48f6c791f86

--- admin/privilege.php
+++ admin/privilege.php
@@ -58,7 +58,7 @@ if ($_REQUEST['act'] == 'login')
         $smarty->assign('gd_version', gd_version());
         $smarty->assign('random',     mt_rand());
     }
-
+    print_r($_SESSION);exit;
     $smarty->display('login.htm');
 }
 
@@ -145,10 +145,12 @@ elseif ($_REQUEST['act'] == 'signin')
 
         ecs_header("Location: ./index.php\n");
 
+        print_r($_SESSION);
         exit;
     }
     else
     {
+        print_r($_SESSION);
         sys_msg($_LANG['login_faild'], 1);
     }
 }
