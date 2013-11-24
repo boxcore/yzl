382774d157ee167663ee421d998de380459c7a03
boxcore <boxcore@f188abb9-0070-40a5-a00a-d48f6c791f86>
2013-11-24 16:30:08 +0000

    git-svn-id: http://www.svnchina.com/svn/noface@51 f188abb9-0070-40a5-a00a-d48f6c791f86

--- themes/yzl_v3/js/base.js
+++ themes/yzl_v3/js/base.js
@@ -1,14 +1,34 @@
 //页面公用类js
-jQuery(document).ready(function() {
-	$("ul.nav_inner li").hover(
-	    function(){
-	        $(this).addClass("one_menu_hover");
-	    },function() {
-	        $(this).removeClass("one_menu_hover");
-	    }
-	);
 
-});
+$(document).ready(function () {
+    $("#navmenu ul li:has(div.two_menu)").hover(function () {
+    	// alert(1111);
+        // $(this).children("a").css({color: "#fff"});
+        if ($(this).find("div.two_menu").length > 0) {
+            // $(this).children("div.two_menu").stop(true, true).slideDown(100)
+            $(this).addClass("one_menu_hover");
+            $('.two_menu', this).attr({style:'display:block;'});
+        }
+    }, function () {
+        // $(this).children("a").css({color: "#fff"});
+        // $(this).children("div.two_menu").stop(true, true).slideUp("fast")
+        $(this).removeClass("one_menu_hover");
+        $('.two_menu', this).attr({style:'display:none;'});
+    });
+})
+
+// jQuery(document).ready(function() {
+// 	$("ul.nav_inner li").hover(
+// 	    function(){
+// 	        $(this).addClass("one_menu_hover");
+// 	    },function() {
+// 	        $(this).removeClass("one_menu_hover");
+// 	    }
+// 	);
+
+// });
+// 
+// 
 // $("ul.nav_inner li").hover(
 //     function(){
 //         $(this).addClass("one_menu_hover");
